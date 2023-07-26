feeaffccb9d532b65f26c79258c87bfcaf643108
Gneissy <ahmetserteserr@gmail.com>
2023-07-26 15:55:50 +0300

    add lobby func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,33 +7,49 @@ const socket = io.connect("http://localhost:3001");
 
 function App() {
 
-
+  // States
   const [message, setMessage] = useState("");
   const [messagesReceived, setMessagesReceived] = useState([]);
+  const [room, setRoom] = useState("");
+
+
+  // Event Handlers
   const handleInputChange = function(event){ setMessage(event.target.value); }
+  const handleRoomChange = function(event){ setRoom(event.target.value); }
 
 
+  // Socket related (They are like api calls to socket)
   const sendMessage = function(){
-    // It is like an api call to the socket
-    socket.emit("SEND_MESSAGE", {message: message})
+    socket.emit("SEND_MESSAGE", { message, room })
+  }
+  const joinRoom = function(){
+    if (room) socket.emit("JOIN_ROOM", room)
   }
 
-  // console.log(message);
 
+  // useEffects
   useEffect(function(){
     socket.on("RECEIVE_MESSAGE", function(data){
       setMessagesReceived([...messagesReceived, data]);
     })
   }, [socket]);
 
+
+  //! Here is a bug causing rerender according to the number of sockets, when i set the code below 
+  // setMessagesReceived(previousMessagesReceived => [...previousMessagesReceived, data]);
+
+
   // To display data taken from socket
   const allMessages = messagesReceived.map((msg)=> <h2>{msg.message}</h2>)
 
-  console.log(messagesReceived);
+
   return (
     <div className="App">
+      <input placeholder = "room" onChange = {handleRoomChange} value = {room} />
+      <button onClick = {joinRoom} > Join Room </button>
+
       <input placeholder = "message" onChange = {handleInputChange} value = {message} />
-      <button onClick = {sendMessage} > Send </button>
+      <button onClick = {sendMessage} > Send Message </button>
       {allMessages}
       
     </div>
--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const { Server } = require("socket.io");
 
 app.use(cors());
 
-// This is how we create an http server with socketio
+// This is how we create an http server for socketio
 const server = http.createServer(app);
 
-
-// Lets run socketio
+// Lets run socketio as our server
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -18,25 +17,21 @@ const io = new Server(server, {
     }
 });
 
-// The thing important for socket io is 
-// We either listen for an event,
-// or emit an event (spread, send)
 //* In my opinion, it's like get and post requests, like get is listen and post is emit.
-
+// on -> for listening an event, and getting data
+// emit -> for sending data for a particular event
 
 io.on("connection", function(socket){
     console.log("User is connected with this id: " + socket.id);
 
+    socket.on("JOIN_ROOM", function(data){
+        socket.join(data);
+    })
+
     socket.on("SEND_MESSAGE", function(data){
-        // console.log(data); // Now instead logging the data, send the message to everyone but myself
-        // I'll use "broadcast" to achieve "everyone but myself"
-        socket.broadcast.emit("RECEIVE_MESSAGE", data);
+        socket.to(data.room).emit("RECEIVE_MESSAGE", data);
     });
 });
-
-
-
-
 
 server.listen(3001, function(){
     console.log("Server is running on 3001");
